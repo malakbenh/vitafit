0288bca3a707cd8ca5d33bdc832a28bd71949281
Amanidounia12 <guerzizamanidounia@gmail.com>
2024-05-18 15:56:55 +0200

    healthy

--- lib/view/screens/store/healthy_product/home_screen.dart
+++ lib/view/screens/store/healthy_product/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:vitafit/view/const/app_const.dart';
-import 'package:vitafit/view/screens/store/healthy_product/SubPages/healthy_product.dart';
-import 'package:vitafit/view/custom_widgets/main_product.dart';
+import 'package:vitafit/const/app_const.dart';
+import 'package:vitafit/screens/pages/SubPages/healthy_product.dart';
+import 'package:vitafit/widget/main_product.dart';
 import 'package:card_swiper/card_swiper.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,88 +10,87 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Positioned(
-                child: Image.asset(
-                  "assets/images/other/Ellipse 27.png",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  child: Image.asset(
+                    "asset/images/Ellipse 27.png",
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.only(
-                          top: 50, left: 30, right: 30, bottom: 90),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                prefix: const SizedBox(
-                                  width: 15,
-                                ),
-                                contentPadding: const EdgeInsets.all(1),
-                                suffixIcon: const Icon(Icons.search),
-                                hintText: "Search",
-                                fillColor: Colors.white,
-                                filled: true,
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(
+                            top: 50, left: 30, right: 30, bottom: 90),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  prefix: const SizedBox(
+                                    width: 15,
+                                  ),
+                                  contentPadding: const EdgeInsets.all(1),
+                                  suffixIcon: const Icon(Icons.search),
+                                  hintText: "Search",
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                          const SizedBox(width: 10),
-                          IconButton(
-                            icon: const Icon(
-                              Icons.notifications_active,
-                              color: Colors.white,
-                              size: 26,
+                            const SizedBox(width: 10),
+                            IconButton(
+                              icon: const Icon(
+                                Icons.notifications_active,
+                                color: Colors.white,
+                                size: 26,
+                              ),
+                              onPressed: () {},
                             ),
-                            onPressed: () {},
-                          ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          CustomProductWidget(
+                              onTap: () {},
+                              imagePath: "asset/images/home/pngwing 2.png",
+                              title: "    Healthy \n Products"),
+                          CustomProductWidget(
+                              onTap: () {},
+                              imagePath: "asset/images/home/pngwing 4.png",
+                              title: " Sport\n Materials"),
+                          CustomProductWidget(
+                              onTap: () {},
+                              imagePath: "asset/images/home/imagesh.png",
+                              title: "  Sport \n swear"),
                         ],
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        CustomProductWidget(
-                            onTap: () {},
-                            imagePath: "assets/images/home/pngwing 2.png",
-                            title: "    Healthy \n Products"),
-                        CustomProductWidget(
-                            onTap: () {},
-                            imagePath: "assets/images/home/pngwing 4.png",
-                            title: " Sport\n Materials"),
-                        CustomProductWidget(
-                            onTap: () {},
-                            imagePath: "assets/images/home/imagesh.png",
-                            title: "  Sport \n swear"),
-                      ],
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 150,
-            child: Swiper(
-              autoplay: true,
-              itemBuilder: (BuildContext context, int index) {
-                return Image.asset(Banners.banners[index]);
-              },
-              itemCount: Banners.banners.length,
+              ],
             ),
-          ),
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.zero,
+            SizedBox(
+              height: 150,
+              child: Swiper(
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index) {
+                  return Image.asset(Banners.banners[index]);
+                },
+                itemCount: Banners.banners.length,
+              ),
+            ),
+            Column(
               children: [
                 Padding(
                   padding:
@@ -178,10 +177,13 @@ class HomeScreen extends StatelessWidget {
                     },
                   ),
                 ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.width * 0.25,
+                ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
